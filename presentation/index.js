f6fdac6b1f552cae498ba35d22d3dc2cc84ec792
Damodar Lohani <dlohani48@gmail.com>
2017-12-21 20:29:20 +0545

    basic presentation

--- presentation/index.js
+++ presentation/index.js
@@ -11,20 +11,25 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Fill,
+  Layout
 } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
+const images = {
+  ionicLogo: require('../assets/ionic.png'),
+  reactNativeLogo: require('../assets/react-native.svg')
+}
+
 // Require CSS
 require("normalize.css");
+require("./styles.css");
 
 const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
+  primary: '#ff4081'
 }, {
   primary: "Montserrat",
   secondary: "Helvetica"
@@ -33,33 +38,94 @@ const theme = createTheme({
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck controls={false} transition={["slide"]} progress="bar" contentHeight={800} transitionDuration={500} theme={theme}>
+        <Slide bgColor="tertiary">
+          <Heading size={1} fit caps textColor="primary" lineHeight={1} >
+            React Native <span style={{color: "#444"}}>VS</span> Ionic
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="secondary" size={6}>
+            JS On Mobile
+          </Text>
+          <Layout>
+            <Fill>
+              <img src={images.reactNativeLogo} />
+            </Fill>
+            <Fill>
+              <img src={images.ionicLogo} />
+            </Fill>
+          </Layout>
+          <Text margin="40px" bold>by Damodar Lohani</Text>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Heading size={2} fit caps textColor="primary">Why they Matter?</Heading>
+          <Text size={6} margin="30px 0 0" textColor="tertiary">
+            Want to develop mobile applications for both Android and iOS but don't want to spend huge amount on each specific platform.
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+
+        <Slide bgColor="primary" textColor="tertiary" bgDarken={0.2}>
+          <Heading size={2} fit caps textColor="secondary">How Do They Run?</Heading>
+              <Heading size={6}>React Native</Heading>
+              <List>
+                <ListItem>
+                  Native app, JS code for UI compiles to Native code with JS logic running on a JS thread hosted by React Native App.
+                </ListItem>
+                <ListItem>
+                  Uses React for development.
+                </ListItem>
+              </List>
+              <Heading size={6}>Ionic</Heading>
+              <List>
+                <ListItem>
+                  Web app running on a web view wrapped by Native app that cccess native features via cordova using cordova plugins.
+                </ListItem>
+                <ListItem>
+                  Uses Angular for development.
+                </ListItem>
+              </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="tertiary">
+          <Heading size={2} fit caps textColor="primary">Cross Platform Compatibility</Heading>
+          <Layout>
+            <Fill>
+              <Heading size={6}>React Native</Heading>
+              <Text>Learn once, Write Everywhere</Text>
+            </Fill>
+            <Fill>
+              <Heading size={6}>Ionic</Heading>
+              <Text>Write Once, Run Everywhere</Text>
+            </Fill>
+          </Layout>
         </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="tertiary">
+          <Heading size={2} fit caps textColor="primary">Cross Platform Styling</Heading>
+          <Layout>
+            <Fill>
+              <Heading size={6}>React Native</Heading>
+              <Text>Build own styling</Text>
+              <Text>However there are third party libraries that provide components styled for specific platforms like nativebase, react native elements..</Text>
+            </Fill>
+            <Fill>
+              <Heading size={6}>Ionic</Heading>
+              <Text>Provides a large set of components that are already styled for each platform. The components are styled with platform specific styles.</Text>
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
+          <Heading size={2} fit caps>Components Library</Heading>
+          <Layout>
+            <Fill>
+              <Heading size={6}>React Native</Heading>
+              <Text>Only basic set of  prebuilt components like View, Text, TextInput</Text>
+              <Text>More advance components we can build using basic components or use third party open source libraries like nativebase, react-native-elements</Text>
+            </Fill>
+            <Fill>
+              <Heading size={6}>Ionic</Heading>
+              <Text>Large collection of prebuilt components including most frequently used advance components like card, action sheets, FABs, Menus, Navigation and so on</Text>
+            </Fill>
+          </Layout>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
